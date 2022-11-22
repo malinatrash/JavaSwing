5054b9143d90a939a270e50b9293252f81d484df
malinatrash <malinatrashh@gmail.com>
2022-11-22 15:52:52 +0800

    Added first abstract class Organization

--- src/malinatrash/Main.java
+++ src/malinatrash/Main.java
@@ -3,6 +3,6 @@ package malinatrash;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
---
+++ src/malinatrash/Organization.java
@@ -0,0 +1,13 @@
+package malinatrash;
+
+public abstract class Organization {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void processOperation();
+
+}

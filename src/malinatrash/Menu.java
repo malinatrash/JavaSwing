753b3e7ec198d66e67412c0d178053fd8876292e
malinatrash <malinatrashh@gmail.com>
2022-11-30 23:21:11 +0800

    added STATE

--- src/malinatrash/Menu.java
+++ src/malinatrash/Menu.java
@@ -1,12 +1,10 @@
 package malinatrash;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
 
     private static final Scanner scanner = new Scanner(System.in);
-    private static final ArrayList<Organization> companies = new ArrayList<Organization>();
 
     public static void start() {
         getMainMenu();
@@ -20,22 +18,31 @@ public class Menu {
             System.out.println("2 - Посмотреть список компаний");
             System.out.println("3 - Выполнить действия всех компаний");
             System.out.println("4 - Выполнить действие конкретной компании");
+            System.out.println("5 - Удалить компанию по имени");
             System.out.println("0 - Выход из программы\n");
 
             int choice = readChoice();
+            String title;
 
             switch (choice) {
                 case 1:
                     createCompany();
                     break;
                 case 2:
-                    getCompaniesList();
+                    State.getCompaniesList();
                     break;
                 case 3:
-                    doAllActivities();
+                    State.doAllActivities();
                     break;
                 case 4:
-                    doSelectedActivity();
+                    System.out.print("Введите название компании, действие которой вы хотите посмотреть: ");
+                    title = scanner.nextLine();
+                    State.doSelectedActivity(title);
+                    break;
+                case 5:
+                    System.out.print("Введите название компании, которую вы хотите удалить: ");
+                    title = scanner.nextLine();
+                    State.deleteSelectedCompany(title);
                     break;
                 case 0:
                     working = false;
@@ -44,64 +51,30 @@ public class Menu {
         } while (working);
     }
 
-    private static void doSelectedActivity() {
-        System.out.print("Введите название компании, действие которой вы хотите посмотреть: ");
-        String title = scanner.nextLine();
-
-        for (Organization company: companies) {
-            if (company.getName() == null ? title == null : company.getName().equals(title)) {
-                company.processOperation();
-                return;
-            }
-        }
-        System.out.println("Компании с выбранным именем не существует!");
-    }
-
-    private static void doAllActivities() {
-        if (companies.isEmpty()) {
-            System.out.println("Для этого добавьте хотя бы одну компанию...");
-            return;
-        }
-        System.out.println();
-        for (Organization company: companies) {
-            company.processOperation();
-        }
-        System.out.println();
-    }
-
-    private static void getCompaniesList() {
-        System.out.println();
-        for (int i = 0; i < companies.size(); i++) {
-            System.out.println((i + 1) + " - " + companies.get(i).getName());
-        }
-        System.out.println();
-    }
-
     private static void createCompany() {
         System.out.println("1 - Страховая компания");
         System.out.println("2 - Судостроительная компания");
         System.out.println("3 - Авиастроительная компания");
         System.out.print("Какую компанию вы хотите создать: ");
 
-        int choice = readChoice();
+        int choice = Menu.readChoice();
         switch (choice) {
             case 1:
                 System.out.print("Введите название компании: ");
-                companies.add(new InsuranceCompany(scanner.nextLine()));
+                State.companies.add(new InsuranceCompany(scanner.nextLine()));
                 return;
             case 2:
                 System.out.print("Введите название компании: ");
-                companies.add(new ShipbuildingCompany(scanner.nextLine()));
+                State.companies.add(new ShipbuildingCompany(scanner.nextLine()));
                 return;
             case 3:
                 System.out.print("Введите название компании: ");
-                companies.add(new AircraftFactory(scanner.nextLine()));
+                State.companies.add(new AircraftFactory(scanner.nextLine()));
                 return;
             default:
                 createCompany();
         }
     }
-
 
     private static int readChoice() {
         Scanner in = new Scanner(System.in);
@@ -114,5 +87,4 @@ public class Menu {
         }
         return readed;
     }
-
 }
---
+++ src/malinatrash/State.java
@@ -0,0 +1,50 @@
+package malinatrash;
+
+import java.util.ArrayList;
+
+public class State {
+
+    protected static final ArrayList<Organization> companies = new ArrayList<Organization>();
+
+    public static void deleteSelectedCompany(String name) {
+        for (Organization company: companies) {
+            if (company.getName() == null ? name == null : company.getName().equals(name)) {
+                companies.remove(company);
+                System.out.println("Успешно удалено!");
+                return;
+            }
+        }
+        System.out.println("Компании с выбранным именем не существует!");
+    }
+
+    public static void doSelectedActivity(String name) {
+
+        for (Organization company: companies) {
+            if (company.getName() == null ? name == null : company.getName().equals(name)) {
+                company.processOperation();
+                return;
+            }
+        }
+        System.out.println("Компании с выбранным именем не существует!");
+    }
+
+    public static void doAllActivities() {
+        if (companies.isEmpty()) {
+            System.out.println("Для этого добавьте хотя бы одну компанию...");
+            return;
+        }
+        System.out.println();
+        for (Organization company: companies) {
+            company.processOperation();
+        }
+        System.out.println();
+    }
+
+    public static void getCompaniesList() {
+        System.out.println();
+        for (int i = 0; i < companies.size(); i++) {
+            System.out.println((i + 1) + " - " + companies.get(i).getName());
+        }
+        System.out.println();
+    }
+}

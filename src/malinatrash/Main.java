8fb5a984f38af1d92d46291e270042066ded7760
malinatrash <malinatrashh@gmail.com>
2023-02-15 21:00:56 +0800

    Добавил главное View.

--- src/malinatrash/Main.java
+++ src/malinatrash/Main.java
@@ -1,8 +1,11 @@
 package malinatrash;
 
+import malinatrash.GUI.ViewControllers.ViewController;
+
 public class Main {
 
     public static void main(String[] args) {
+        new ViewController();
         Menu.start();
     }
 }
